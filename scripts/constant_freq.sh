6e1e719c38cc7c4410edfa75e1df2c8a50bb47de
jerry <jerryzhang19257@gmail.com>
2026-06-09 20:28:57 -0600

    update

--- scripts/constant_freq.sh
+++ scripts/constant_freq.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
-set -e
+set +e
 CPU_FREQ_KHZ=2500000
-#RDMSR=$(which rdmsr)
-#WRMSR=$(which wrmsr)
-#echo $RDMSR
-#echo $WRMSR
-# Get the input (e.g., 3.25GHz) and convert to uppercase for easier matching
 INPUT=$(echo "$1" | tr '[:lower:]' '[:upper:]')
 
 # Extract the numeric part (including decimals)
@@ -50,31 +45,8 @@ disable_cstate() {
 	for i in $(ls /sys/devices/system/cpu/cpu*/cpuidle/state*/disable); do echo "1" | sudo tee $i > /dev/null 2>&1 ;done
 }
 
-# Let us be normal ....
 disable_turbo() {
-
 	echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
-
-#	if ! [ -x "$(command -v ${RDMSR})" ]; then
-#		echo "Installing msr-tools ..."
-#		sudo apt update && sudo apt install msr-tools
-#		RDMSR=$(which rdmsr)
-#		WRMSR=$(which wrmsr)
-#	fi
-#
-#	echo "Loading msr module"
-#	sudo modprobe msr
-#
-#	# make sure we have this module loaded
-#	if [ -z "$(lsmod | grep '^msr')" ]; then
-#		echo "ERROR: Fail to load msr module into kernel!"
-#		exit
-#	fi
-#
-#	# disable turbo boost (bit 38 on 0x1a0 msr)
-#	TURBO_BOOST_BIT=38
-#	echo "Disabling turboboost"
-#	sudo $WRMSR -a 0x1a0 "$(printf '0x%x' "$(( $(sudo $RDMSR -d 0x1a0) | (1 << TURBO_BOOST_BIT) ))")"
 }
 
 set_const_freq() {
